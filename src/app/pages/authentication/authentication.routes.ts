21407388a02a72716526a67b12f7c53efa077135
Flerys11 <fleuryssh@gmail.com>
2026-03-03 22:30:53 +0300

    login admin

--- src/app/pages/authentication/authentication.routes.ts
+++ src/app/pages/authentication/authentication.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { AppSideLoginComponent } from './side-login/side-login.component';
 import { AppSideRegisterComponent } from './side-register/side-register.component';
+import { LoginAdminComponent} from "./login-admin/login-admin.component";
 
 export const AuthenticationRoutes: Routes = [
   {
@@ -14,6 +15,11 @@ export const AuthenticationRoutes: Routes = [
       {
         path: 'register',
         component: AppSideRegisterComponent,
+      },
+
+      {
+        path: 'login_admin',
+        component: LoginAdminComponent,
       },
     ],
   },
